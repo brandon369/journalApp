27ce4af27ffbe7f1656281f8f7e2e43255a86313
Brandon A <byarrieta@protonmail.com>
2021-10-10 16:09:42 -0500

    get is auth for firebase

--- src/routers/AppRouter.js
+++ src/routers/AppRouter.js
@@ -1,9 +1,29 @@
-import React from 'react'
+import React, {useEffect} from 'react'
 import {Route, BrowserRouter as Router, Switch, Redirect} from "react-router-dom";
 import AuthRouter from "./AuthRouter";
 import JournalScreen from "../components/journal/JournalScreen";
+import {onAuthStateChanged, getAuth} from 'firebase/auth'
+import {useDispatch} from "react-redux";
+import {login} from "../actions/auth";
 
 const AppRouter = () => {
+
+  const dispatch = useDispatch()
+
+  useEffect(() => {
+    const auth = getAuth();
+    onAuthStateChanged(auth, (user) => {
+      console.log(user);
+
+      if (user?.uid) {
+        dispatch(login(user.uid, user.displayName))
+      }
+
+
+    })
+  }, [dispatch]);
+
+
   return (
     <Router>
       <div>
